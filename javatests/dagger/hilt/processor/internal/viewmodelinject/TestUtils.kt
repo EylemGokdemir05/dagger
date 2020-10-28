a3f945a94dae972d26f214b15e8b544b4cc30ae9
Dagger Team <java-team-github-bot@google.com>
2020-10-28 16:50:31 -0700

    Migrating @ViewModelInject into Hilt's codebase.
    This CL brings the ViewModel extension from AndroidX into Hilt's codebase to provide first part support for ViewModels within Hilt. It does not update the mechanism for which the HiltViewModelFactory is provided into the @AndroidEntryPoint-annotated classes.
    
    RELNOTES=Migrating @ViewModelInject into Hilt's codebase.
    PiperOrigin-RevId: 339565069

---
+++ javatests/dagger/hilt/processor/internal/viewmodelinject/TestUtils.kt
@@ -0,0 +1,19 @@
+package dagger.hilt.processor.internal.viewmodelinject
+
+import com.google.testing.compile.Compiler
+import com.google.testing.compile.JavaFileObjects
+import dagger.hilt.android.processor.internal.viewmodelinject.ViewModelInjectProcessor
+
+internal val GENERATED_TYPE = try {
+  Class.forName("javax.annotation.processing.Generated")
+  "javax.annotation.processing.Generated"
+} catch (_: ClassNotFoundException) {
+  "javax.annotation.Generated"
+}
+
+internal val GENERATED_ANNOTATION =
+  "@Generated(\"dagger.hilt.android.processor.internal.viewmodelinject.ViewModelInjectProcessor\")"
+
+internal fun String.toJFO(qName: String) = JavaFileObjects.forSourceString(qName, this.trimIndent())
+
+internal fun compiler(): Compiler = Compiler.javac().withProcessors(ViewModelInjectProcessor())
